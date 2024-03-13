85c25e592496945b67cc4740d0057fc0e49b5909
massoncorlette <massoncorlette07@gmail.com>
2024-03-13 00:11:34 -0400

    clean default

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
+let addedNumber;
 let currentNumber;
-let addedNum;
-let operator;
 
-function display(currentNum) {
+function displayScreen() {
   let display = document.querySelector('.screen');
   let clear = document.querySelector('#c');
-  let allBtns = document.querySelectorAll('btn');
   let btnsNums = document.querySelectorAll('.num')
   // using .forEach method instead of for loop
   btnsNums.forEach((button) => {
     button.addEventListener('click', () => {
       display.textContent += button.textContent;
-      currentNum += display.textContent;
+      addedNumber += button.textContent;
+      console.log(addedNumber);
     });
   });
   // the delete listener to clear variables and screen
   clear.addEventListener('click',() => {
     display.textContent = null;
+    addedNumber = null;
     currentNumber = null;
-    currentNum = null;
   })
-  // adding the pressed numbers to global var
-  currentNumber += currentNum;
 } 
-display();
-console.log(currentNumber);
+displayScreen();
 
-function operateResult(currentNum,numTwo,functionCallBack) {
-  let display = document.querySelector('.screen');
-  display.textContent = null;
-  display(numTwo);
-  return functionCallBack(currentNum,numTwo);
+function operate() {
+
 }
+
+
 
 function add(valueOne, valueTwo) {
 	return (valueOne + valueTwo);
@@ -41,20 +36,15 @@ function sub(valueOne, valueTwo) {
 	return (valueOne - valueTwo);
 };
 
-function addSum(values) {
-  return values.reduce((currentTotal, currentNum) => currentTotal + currentNum);
-};
-
-function multiply(values) {
- return values.reduce((currentTotal, currentNum) => currentTotal * currentNum);
+function multiply(valueOne, valueTwo) {
+ return (valueOne * valueTwo);
 };
 
 function power(valueOne, valueTwo) {
-  return (valueOne ** valueTwo)
+  return (valueOne ** valueTwo);
 };
 
 const adding = document.querySelector('#add')
-adding.addEventListener('click',operateResult() );
 
 
 
